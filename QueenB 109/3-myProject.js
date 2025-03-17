ae7678cc9aa6614604a808eea871b9b32c83b83e
Maya Katan <135252531+MayaKatan@users.noreply.github.com>
2025-03-17 19:39:00 +0200

    Add files via upload

---
+++ QueenB 109/3-myProject.js
@@ -0,0 +1,24 @@
+function spelling_test_word1() {
+    var answer = prompt("איך אומרים באנגלית בננה?");
+    while (answer != "banana") {
+      answer = prompt("try again:");
+    }
+    alert("You were rigth!");
+  }
+  
+  function spelling_test_word2() {
+    var answer = prompt("איך אומרים באנגלית תות?");
+    while (answer != "strawberry") {
+      answer = prompt("try again:");
+    }
+    alert("You were rigth!");
+  }
+  
+  function spelling_test_word3() {
+    var answer = prompt("איך אומרים באנגלית תפוז?");
+    while (answer != "orange") {
+      answer = prompt("try again:");
+    }
+    alert("You were rigth!");
+  }
+  
